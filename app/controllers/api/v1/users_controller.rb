e16e722a6cc418f8d9ab78f133c7bbab3e17fbdc
yamunanavada <yamunanavada@gmail.com>
2018-03-07 17:15:08 -0500

    updated css

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,7 +18,6 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       token = encode_token({ user_id: @user.id })
-      byebug
       render json: {user: @user, jwt: token},  status: 200
     else
       render json: {errors: @user.errors.full_messages}, status: 422
